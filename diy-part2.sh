93dfc95a79935e7435ac8c86d7d990beb56bffa8
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2022-12-03 00:40:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,17 +11,38 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# Modify default IP Gate #
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 echo '### Updates default IP gate ###'
 
-# echo '### Updates Theme Argon ###'
+# Updates Theme Argon #
 # package/lean/luci-theme-argon /package/feeds/luci/luci-theme-argon
 rm -rf package/feeds/luci/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
-echo '###  ###'
+echo '# Updates Theme Argon #'
 
-# echo '### Updates H5962 Flash Memory Space ###'
+# Argon Theme Config plug-in components #
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+echo '# Argon Theme Config plug-in components #'
+
+# Updates H5962 Flash Memory Space #
 rm -rf lede/target/linux/ramips/image/mt7621.mk
 wget -P lede/target/linux/ramips/image/ https://github.com/ZHOUJUNX/OpenWRT-HiWiFi-H5962-ssr/blob/main/mt7621.mk
-echo '###  ###'
+echo '# Updates H5962 Flash Memory Space #'
+
+# luci-app-adguardhome plug-in components #
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
+echo '# luci-app-adguardhome plug-in components #'
+
+# luci-app-smartdns plug-in components #
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
+echo '# luci-app-smartdns plug-in components #'
+
+# OpenAppFilter plug-in components #
+# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# echo '# OpenAppFilter plug-in components #'
+
+
+# Shutdown plug-in components #
+# git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
+# echo '# Shutdown plug-in components #'
